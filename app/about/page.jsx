741a51f5ac8e45d6a31b183532eef714f0c3497a
Stanley Zheng <stanleyzhengs@gmail.com>
2025-01-07 18:47:06 -0500

    About Page: responsive layout, Image Carousel Update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,5 @@
 import ReviewCarousel from "../components/ReviewCarousel "
 
-
-
 const IMAGES = [
   "./images/BR1.png",
   "./images/BR2.png", 
@@ -14,9 +12,10 @@ const IMAGES = [
 const page = () => {
   return (
     <div className="flex flex-col items-center font-sans pt-14 border-2">
-      <div className="flex flex-col items-center sm:w-1/2  gap-4" alt="about">
+      {/* About Us Description Container */}
+      <div className="flex flex-col items-center sm:w-1/2 gap-4" alt="about">
         <h1 className="font-bold text-4xl">About Us</h1>
-        <p>
+        <p className="md:text-lg">
           You’re a student, a coffee lover, or work remotely and only care about a tasty 
           drink and a nice ambiance. You go to the reviews to look for recommendations and 
           get hit with a 2-star review life lesson on sustainability. Im all for it but what 
@@ -25,16 +24,19 @@ const page = () => {
           alike explore local coffee spots, read genuine reviews, and share their own experiences
           dedicated strictly to drinks, food, and ambiance.
         </p>
+        <div className="bg-grey p-3 rounded-xl">
+          1 Star Review's Gathered from cafe's with more that 4.5 Star's and 100+ Review's
+        </div>
       </div>
-      <div>
-        Examples
 
-        
-      </div>
+      {/* Image Carousel Container */}
       <div className="w-full sm:w-1/2 pt-10">
         <ReviewCarousel imageUrls = {IMAGES}/>
       </div>
 
+      <div>
+        standards
+      </div>
  
     </div>
   )
--- app/components/ReviewCarousel .jsx
+++ app/components/ReviewCarousel .jsx
@@ -25,10 +25,10 @@ const ReviewCarousel  = ({imageUrls}) => {
 
   return (
     // Container
-    <div className='relative'>
+    <div className='relative group'>
 
       {/* Image content for carousel */}
-      <div className='flex h-full w-full border-2 overflow-hidden '>
+      <div className='flex h-full w-full overflow-hidden '>
         {imageUrls.map(url =>(
           <img
               key = {url}
@@ -40,20 +40,19 @@ const ReviewCarousel  = ({imageUrls}) => {
           />
         ))}
       </div>
+
       {/* Navigation button  */}
       <button 
         onClick={showPrevImage}
-        className=' border-2 absolute top-0 bottom-0 left-0 p-4'>
+        className='hidden group-hover:flex justify-center items-center bg-grey bg-opacity-15 absolute top-1/2 -translate-y-1/2 left-0 p-1 h-20 rounded-r-xl' >
           <FaAngleLeft />
       </button>
       <button 
         onClick={showNextImage}
-        className='border-2 block absolute top-0 bottom-0 right-0 p-4'>
+        className='hidden group-hover:flex justify-center items-center bg-grey bg-opacity-15 absolute top-1/2 -translate-y-1/2 right-0 p-1 h-20 rounded-l-xl'>
           <FaAngleRight />
       </button>
-      
 
-      
     </div>
   )
 }
